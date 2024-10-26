c65f7fc61666dfe91886f20c02617c7d10ca33a9
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-26 19:01:04 +0200

    LAST CHANCE

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,6 +4,7 @@ import requests
 
 
 def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': 'admin'}
     response = requests.get(url, headers=headers)

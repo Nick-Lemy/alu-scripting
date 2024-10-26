930d7f02192e88dbd79228df05388a4617e24706
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-26 17:01:44 +0200

    Refactor number_of_subscribers function to handle non-200 response codes

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,12 +2,16 @@
 """
 reddit api
 """
+
 import requests
+
+
 def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r/{}/about.json'
-    data = requests.get(url.format(subreddit), headers=headers).json()
-    try:
-        return data["data"]['subscribers']
-    except:
+    response = requests.get(url.format(subreddit), headers=headers)
+    data = response.json()
+    if response.status_code != 200:
         return 0
+    else:
+        return data['data']['subscribers']

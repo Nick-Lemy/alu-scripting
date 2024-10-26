e7135e72bf891848ee4a9a3c44b1ac1bd605bed7
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-26 18:49:58 +0200

    ok

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,32 +2,16 @@
 """
 Module
 """
-
-import json
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-agent': 'myRedditScript/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
+    """Returns the top ten posts for a given subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'admin'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            top_posts = [post['data']['title']
-                         for post in data['data']['children']]
-            for post_title in top_posts:
-                print(post_title)
-        else:
-            print("No posts found")
+        for i in response.json().get("data").get("children"):
+            print(i.get("data").get("title"))
     else:
-        print("None")
-
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+        print(None)

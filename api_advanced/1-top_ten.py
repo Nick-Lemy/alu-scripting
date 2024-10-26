0a765da1ffffd8522e624f1da1d85ac4384395dc
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-26 18:30:44 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,36 @@
+
 #!/usr/bin/python3
-
 """
-reddit api
+This module contains the function top_ten.
 """
-
-
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
     """
-    prints titles of 10 hotest post if @subreddit is valid subreddit.
-    if not print None.
+    Returns the top ten posts for a given subreddit.
     """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'admin'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        for i in response.json().get("data").get("children"):
-            print(i.get("data").get("title"))
+    user = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
+
+    try:
+        response = requests.get(url, headers=user, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json()
+
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        print("OK")
+    except requests.exceptions.RequestException:
+        print("None")
+    except ValueError:
+        print("None")
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        top_ten(argv[1])
     else:
-        print(None)
+        print("Usage: ./script.py <subreddit>")

ca2926debfc72fbb6f3fc19fb0e245cfcf31823f
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-26 19:06:29 +0200

    ok

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,11 +5,12 @@ import requests
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'admin'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        for i in response.json().get("data").get("children")[0:10]:
-            print(i.get("data").get("title"))
-    else:
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])

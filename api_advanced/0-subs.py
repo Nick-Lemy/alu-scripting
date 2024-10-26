e6e45c37c69d40d89efa65dac29e5531466f0ae8
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-26 17:29:56 +0200

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,10 +7,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'My User Agent 1.0'}
-    url = 'https://www.reddit.com/r/{}/about.json'
-    response = requests.get(url.format(subreddit), headers=headers)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers)
     data = response.json().get('data')
-    if response.status_code == 200:      
+    if response.status_code == 200:
         return data.get('subscribers')
     else:
         return 0

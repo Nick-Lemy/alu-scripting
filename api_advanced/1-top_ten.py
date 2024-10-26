56594e804652e0a19f985c7d19dd45204f108ed5
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-26 19:09:09 +0200

    None

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        print('OK')
+        print(None)
         return
     posts = response.json()['data']['children']
     for post in posts:
